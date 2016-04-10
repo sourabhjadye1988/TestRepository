5b44f99b23d44a10ffd034dbb5d9d2cbeb2f7200
sourabhjadye1988 <sourabhjadye@gmail.com>
2016-04-10 22:39:26 +0530

    Log4Net Sample
    Utility Added with Example as Console Application

---
+++ Utilities/ConsoleApplication1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using Utilities;
+
+namespace Log4NetTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Trace.Write("Trece Message", "Info Trace");
+            for (int i = 0; i < 2500; i++)
+            {
+                Logger.Log("Error Message", Level.ERROR);
+                Logger.Log("Info Message", Level.INFO);
+                Logger.Log("WARN Message", Level.WARN);
+                Logger.Log("Debug Message", Level.DEBUG);
+                Logger.Log("Fatal message", Level.FATAL);
+            }
+            Logger.Log("========================FINISH==========================", Level.INFO);
+            Console.Read();
+        }
+    }
+}
---
+++ Utilities/Logger/Logger.cs
@@ -0,0 +1,63 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Utilities
+{
+    //Log levels
+    public enum Level
+    {
+        INFO = 0,
+        DEBUG = 1,
+        ERROR = 2,
+        FATAL = 3,
+        WARN = 4
+    }
+
+    public class Logger
+    {
+        //Init
+        private static readonly ILog logger =
+            LogManager.GetLogger(typeof(Logger));
+
+        //Constructor
+        static Logger()
+        {
+            GlobalContext.Properties["formattedDate"] = DateTime.Now.Month + "_" + DateTime.Now.Day + "_" + DateTime.Now.Year;
+            XmlConfigurator.Configure();
+            //DOMConfigurator.Configure();            
+        }
+        
+        //Loggin Function
+        public static void Log(string message, Level level = Level.ERROR)
+        {
+            switch (level)
+            {
+                case Level.INFO:
+                    logger.Info(message);
+                    break;
+                case Level.DEBUG:
+                    logger.Debug(message);
+                    break;
+                case Level.ERROR:
+                    logger.Error(message);
+                    break;
+                case Level.FATAL:
+                    logger.Fatal(message);
+                    break;
+                case Level.WARN:
+                    logger.Warn(message);
+                    break;
+                default:
+                    
+                    break;
+            }
+        }
+
+        
+    }
+}
